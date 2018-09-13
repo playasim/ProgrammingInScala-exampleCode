69f6e5f53e2371f99a1fed7b3f8863199570c768
MartinMa <maang_ouc@163.com>
2018-09-13 19:48:03 +0800

    Add above() beside() method

--- src/main/scala/chapter10/Element.scala
+++ src/main/scala/chapter10/Element.scala
@@ -11,5 +11,14 @@ abstract class Element {
   //val的访问会比方法快，但会占用额外空间
   def height: Int = contents.length
   def width: Int = if (height == 0) 0 else contents(0).length
+  def above(that: Element) = {
+    new ArrayElement(this.contents ++ that.contents)
+  }
+  def beside(that: Element) = {
+    val contents = new Array[String](this.contents.length)//假设高度一样
+    for (i <- 0 until this.contents.length)
+      contents(i) = this.contents(i) + that.contents(i)
+    new ArrayElement(contents)
+  }
 
 }
--- src/main/scala/chapter10/LineElement.scala
+++ src/main/scala/chapter10/LineElement.scala
@@ -5,7 +5,8 @@ package chapter10
 @Author Martin Ma
   * @Date 2018/9/12
   **/
-class LineElement(s: String) extends ArrayElement(Array(s)){
+class LineElement(s: String) extends Element {
+  override def contents: Array[String] = Array(s)
   override val height = 1
   override val width = s.length
 }
